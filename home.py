042026a2a00ce063f91a4fc225cbed03b8ea4cc9
Hariharasudhan P <hariharasudhan.p@themathcompany.com>
2022-09-03 15:44:36 +0530

    will snowflake connector

--- home.py
+++ home.py
@@ -1,35 +1,37 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import snowflake.connector
+from matplotlib import pyplot as plt
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-@st.cache      
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
 
-data_load_state = st.text(''' # loading data.... ''')
+def init_connection():
+    return snowflake.connector.connect(**st.secrets["snowflake"])
 
-data = load_data(1000)
+conn = init_connection()
 
-data_load_state.text("Done! (using st.cache)")
+@st.cache
+def data_fetcher():
+    query = '''SELECT * FROM cereals'''
+    cur = conn.cursor().execute(query)
+    snowflake_data = pd.DataFrame.from_records(iter(cur), columns=[x[0] for x in cur.description])
+    return snowflake_data
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+st.write('# Demo App')
 
-# hour_to_filter = 17
-hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-st.map(filtered_data)
+st.write('## Popular Products: ')
+option = st.selectbox(
+     'Select the product type: ',
+     data_fetcher().TYPE.unique())
+
+st.write('Currently Selected:', option)
+
+# df = data_fetcher().sort_values(by=['RATING'], ascending=False)
+# df
+
+df = data_fetcher()
+df1 = df[df['TYPE']==option].sort_values(by=['RATING'], ascending=False).head(15)
+st.bar_chart(data=df1, x='NAME', y='RATING', width=0, height=0, use_container_width=True)
+
